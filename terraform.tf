7728930fe58c69fdb20e8edae88fab6465d8749b
ddangCONG <85064259+ddangCONG@users.noreply.github.com>
2024-10-03 04:17:15 +0900

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source  = "app.terraform.io/capston/terraform/s3-webap/aws"
+      source  = "app.terraform.io/capston/s3-webap/aws"
       version = "4.21.0"
     }
   }
