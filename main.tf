347057cf7361ef3d646840d53fe14da14e4d7206
ddangCONG <85064259+ddangCONG@users.noreply.github.com>
2024-10-03 04:13:43 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/capston/s3-webap/aws"
+  source  = "app.terraform.io/capston/terraform/s3-webap/aws"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
